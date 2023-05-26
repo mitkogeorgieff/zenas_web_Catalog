a3358ed8f34cb6919dfd02548d9e82b2d44ff3c7
Dimitar Georgiev <mitkogeorgieff@gmail.com>
2023-05-26 19:00:34 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
+import streamlit
+import pandas
+import requests
+import snowflake.connector
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets.snowflake)
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
